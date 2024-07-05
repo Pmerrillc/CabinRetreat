16d172acc6db403687162313c23cfeb7843f9b84
mayank2001 <55726377+mayank2001@users.noreply.github.com>
2024-07-05 23:46:18 +0530

    changes 11:46PM

--- src/Pages/BookNow/BookNow.jsx
+++ src/Pages/BookNow/BookNow.jsx
@@ -4,7 +4,7 @@ import HorizontalImageSlider from '../../components/HorizontalImageSlider';
 
 const BookNow = () => {
   return (
-    <div className="min-h-[70vh] flex flex-col justify-center w-full md:w-[100%] mt-20">
+    <div className="min-h-[70vh] flex flex-col justify-center w-full md:w-[100%] mt-10">
       <div className="flex flex-col lg:flex-row lg:space-x-16 items-center justify-center">
         <BookNowText/>
         <HorizontalImageSlider/>
