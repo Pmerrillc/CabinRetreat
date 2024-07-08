d22a5bc8614552569765f9951f66b8b9304534cd
mayank2001 <55726377+mayank2001@users.noreply.github.com>
2024-07-08 23:03:49 +0530

    footer changes

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaFacebookF } from 'react-icons/fa'; 
+import { Link } from 'react-scroll';
 
 const Footer = () => {
   return (
@@ -7,7 +8,7 @@ const Footer = () => {
       <div className="flex flex-col items-start space-y-4 mb-8 md:mb-0 md:pr-16">
         <a href='#'><h1 className="text-2xl font-sans">Cabin10.com</h1></a>
         <a href="https://www.facebook.com/riversresortcabin10?mibextid=LQQJ4d" className="text-white">
-        <FaFacebookF className="text-2xl" />
+          <FaFacebookF className="text-2xl" />
         </a>
       </div>
       
@@ -15,18 +16,28 @@ const Footer = () => {
         <div className="flex flex-col space-y-2 mb-4 md:mb-0">
           <a href="https://www.airbnb.com/slink/4wW5PCQ4" className="text-white hover:text-[#6e6e6e]">AirBnB</a>
           <a href="https://www.riversresortrentals.com/en/rivers-resort-cabin-10" className="text-white hover:text-[#6e6e6e]">Lodgify</a>
-          <a href="#" className="text-white hover:text-[#6e6e6e] ">Contact us</a>
+          <Link to="contact" smooth={true} duration={500} className="text-white hover:text-[#6e6e6e] cursor-pointer">
+            Contact us
+          </Link>
         </div>
         
         <div className="flex flex-col space-y-2">
-          <a href="#" className="text-white hover:text-[#6e6e6e]">Listing description</a>
-          <a href="#" className="text-white hover:text-[#6e6e6e]">Things to do nearby</a>
-          <a href="#" className="text-white hover:text-[#6e6e6e]">Property Details</a>
+          <Link to="section2" smooth={true} duration={500} className="text-white hover:text-[#6e6e6e] cursor-pointer">
+            Listing description
+          </Link>
+          <Link to="section2" smooth={true} duration={500} className="text-white hover:text-[#6e6e6e] cursor-pointer">
+            Things to do nearby
+          </Link>
+          <Link to="section1" smooth={true} duration={500} className="text-white hover:text-[#6e6e6e] cursor-pointer">
+            Property Details
+          </Link>
         </div>
       </div>
       
       <div className="flex items-center mt-8 md:mt-0">
-        <a href="https://www.airbnb.com/slink/4wW5PCQ4" className="py-2 px-4 border-2 bg-white border-red-500 text-red-500 font-bold rounded-full hover:bg-red-500 hover:text-white">Book Now</a>
+        <a href="https://www.airbnb.com/slink/4wW5PCQ4" className="py-2 px-4 border-2 bg-white border-red-500 text-red-500 font-bold rounded-full hover:bg-red-500 hover:text-white">
+          Book Now
+        </a>
       </div>
     </footer>
   );
