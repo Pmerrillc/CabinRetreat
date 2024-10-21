e5c72ba7826b5b95045462de2d3962089bbf623b
mayank2001 <55726377+mayank2001@users.noreply.github.com>
2024-10-21 09:56:43 +0530

    mobile changes

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -10,7 +10,7 @@ const Details = () => {
   const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1024px)' });
 
   return (
-    <Element name="section1" className="min-h-[70vh] flex flex-col justify-center w-full md:w-[100%]">
+    <Element name="section1" className="min-h-[70vh] flex flex-col justify-center w-full md:w-[100%] md:mb-[10vh]">
       <div className="flex flex-col lg:flex-row items-center justify-center">
         {isDesktopOrLaptop ? <VerticalImageSlider /> : <ImageGrid />}
         {isDesktopOrLaptop ? <DetailsText /> : <TextDetails />}
